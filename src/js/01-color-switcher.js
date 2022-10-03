89bcac3f14cd07ca67ab00fab27224424700d16e
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-10-03 21:33:00 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
+const startBtn = document.querySelector(['data-start']);
+const stopBtn = document.querySelector(['data-stop']);
 
+startBtn.addEventListener('click', onClickStart);
+
+function onClickStart(e) {
+  console.log(e);
+}
