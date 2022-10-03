0cf1faac694448d970a45c0eb6bfbe713fa6d70b
Tauzhnianskyi <104843369+Tauzhnianskyi@users.noreply.github.com>
2022-10-03 19:38:31 +0000

    Deploying to gh-pages from @ Tauzhnianskyi/goit-js-hw-09@89bcac3f14cd07ca67ab00fab27224424700d16e 🚀

---
+++ 01-color-switcher.bd36303f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(["data-start"]);document.querySelector(["data-stop"]);t.addEventListener("click",(function(t){console.log(t)}))}();
+//# sourceMappingURL=01-color-switcher.bd36303f.js.map
---
+++ 01-color-switcher.e1058704.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(["data-start"]);document.querySelector(["data-stop"]);t.addEventListener("click",(function(t){console.log(t)}));
+//# sourceMappingURL=01-color-switcher.e1058704.js.map
