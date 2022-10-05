7abb98210d7ba31451d633f3260cd878bc55799d
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-10-05 11:29:52 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,21 @@
-const startBtn = document.querySelector(['data-start']);
-const stopBtn = document.querySelector(['data-stop']);
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId;
 
-startBtn.addEventListener('click', onClickStart);
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
 
-function onClickStart(e) {
-  console.log(e);
+function onStartClick() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.removeEventListener('click', onStartClick);
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+  startBtn.addEventListener('click', onStartClick);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
